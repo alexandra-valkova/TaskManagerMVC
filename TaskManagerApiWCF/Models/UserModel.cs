79767617eabf8b1180fd122504c93934700a3ca0
Alexandra <alexandra.valkova.97@gmail.com>
2016-09-12 14:57:31 +0300

    UserModel changed

--- TaskManagerApiWCF/Models/UserModel.cs
+++ TaskManagerApiWCF/Models/UserModel.cs
@@ -1,15 +1,23 @@
-﻿namespace TaskManagerApiWCF.Models
+﻿using System.Runtime.Serialization;
+
+namespace TaskManagerApiWCF.Models
 {
+    [DataContract]
     public class UserModel : BaseModel
     {
+        [DataMember]
         public string Username { get; set; }
 
+        [DataMember]
         public string Password { get; set; }
 
+        [DataMember]
         public string FirstName { get; set; }
 
+        [DataMember]
         public string LastName { get; set; }
 
+        [DataMember]
         public bool IsAdmin { get; set; }
     }
 }
